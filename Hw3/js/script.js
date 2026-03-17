161a5e349d9810f57f0e81557452210c448c56a5
Sami Hobson <shobson@csumb.edu>
2026-03-17 16:03:31 -0700

    Pushing hw3 to github!

--- Hw3/js/script.js
+++ Hw3/js/script.js
@@ -3,6 +3,7 @@ const nameInput = document.getElementById("name");
 const speciesSelect = document.getElementById("species");
 const results = document.getElementById("results");
 const message = document.getElementById("message");
+const clearBtn = document.getElementById("clearBtn");
 
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
@@ -31,13 +32,13 @@ form.addEventListener("submit", async function (e) {
 
 
     try {
-        message.textContent = "Loading...";
+        message.textContent = "Scanning the multiverse...";
 
         const response = await fetch(url);
 
         if (!response.ok) {
             message.textContent = "";
-            results.innerHTML = '<p class="no-results">No characters found.</p>'
+            results.innerHTML = '<p class="no-results">No characters found. Try Rick, Morty, Summer, Beth, or Jerry!</p>'
             return;
         }
 
@@ -52,18 +53,20 @@ form.addEventListener("submit", async function (e) {
 
             const card = document.createElement("div");
             card.classList.add("card");
-
             card.innerHTML = `
-        <img src="${character.image}" alt="${character.name}">
-        <div class="card-content">
-          <h3>${character.name}</h3>
-          <p><strong>Status:</strong> 
-          <span style="color:${statusColor}">${character.status}</span>
-          </p>
-          <p><strong>Species:</strong> ${character.species}</p>
-          <p><strong>Origin:</strong> ${character.origin.name}</p>
-          </div>
-          `;
+                <img src="${character.image}" alt="${character.name}">
+                <div class="card-content">
+                    <h3>${character.name}</h3>
+                    <p><strong>Status:</strong>
+                        <span style="color:${statusColor}; font-weight:600;">${character.status}</span>
+                    </p>
+                    <p><strong>Species:</strong> ${character.species}</p>
+                    <p><strong>Gender:</strong> ${character.gender}</p>
+                    <p><strong>Origin:</strong> ${character.origin.name}</p>
+                    <p><strong>Location:</strong> ${character.location.name}</p>
+                </div>
+        `;
+
 
             results.appendChild(card);
         });
@@ -72,4 +75,11 @@ form.addEventListener("submit", async function (e) {
         message.textContent = "";
         results.innerHTML = `<p class="no-results">${err.message}</p>`;
     }
+});
+
+clearBtn.addEventListener("click", function () {
+    nameInput.value = "";
+    speciesSelect.value = "";
+    message.textContent = "";
+    results.innerHTML = "";
 });
